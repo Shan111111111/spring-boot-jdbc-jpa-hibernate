bb29c7cd85021569b692b2f4e3a492b54861c3a4
Shantesh <shanthu1804@gmail.com>
2024-07-25 22:00:27 +0530

    added custom Course class to passs the values by course object or memory in spring jdbc

--- src/main/java/com/shantesh/learning/spring_boot_jdbc_jpa_hibernate/jdbc/CourseCommandLineRunner.java
+++ src/main/java/com/shantesh/learning/spring_boot_jdbc_jpa_hibernate/jdbc/CourseCommandLineRunner.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.shantesh.learning.spring_boot_jdbc_jpa_hibernate.pojo.Course;
+
 @Component
 public class CourseCommandLineRunner implements CommandLineRunner {
 
@@ -13,7 +15,11 @@ public class CourseCommandLineRunner implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		courseJDBCRepository.insert();
+		courseJDBCRepository.insert(new Course(234, "Samuel", "Some Author"));
+		courseJDBCRepository.insert(new Course(235, "somevalue", "Some Autdsdsdsdhor"));
+		courseJDBCRepository.insert(new Course(24, "asf", "Some dfdfAuthor"));
+		
+		courseJDBCRepository.delete(235);
 
 	}
 
--- src/main/java/com/shantesh/learning/spring_boot_jdbc_jpa_hibernate/jdbc/CourseJDBCRepository.java
+++ src/main/java/com/shantesh/learning/spring_boot_jdbc_jpa_hibernate/jdbc/CourseJDBCRepository.java
@@ -4,16 +4,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.shantesh.learning.spring_boot_jdbc_jpa_hibernate.pojo.Course;
+
 @Repository
 public class CourseJDBCRepository {
 	
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 	
-	private String INSERT_QUERY="insert into course(id, name, author) values (222, 'shantesh', 'Daniel Steldom')";
+	private String INSERT_QUERY="insert into course(id, name, author) values (?, ?, ?)";
+	private String DELETE_QUERY="DELETE FROM course WHERE id = ?";
 	
-	public void insert() {
-		jdbcTemplate.update(INSERT_QUERY);
+
+	
+	public void insert(Course course) {
+		jdbcTemplate.update(INSERT_QUERY, course.getId(), course.getName(), course.getAuthor());
+	}
+
+
+
+	public void delete(long id) {
+		jdbcTemplate.update(DELETE_QUERY, id);
 	}
 
 }
