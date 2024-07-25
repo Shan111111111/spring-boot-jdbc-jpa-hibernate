2e92e22c5a527cb93403c84447ced6ac13251d3e
Shantesh <shanthu1804@gmail.com>
2024-07-25 21:07:09 +0530

    initial commit

---
+++ src/main/java/com/shantesh/learning/spring_boot_jdbc_jpa_hibernate/SpringBootJdbcJpaHibernateApplication.java
@@ -0,0 +1,14 @@
+package com.shantesh.learning.spring_boot_jdbc_jpa_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJdbcJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJdbcJpaHibernateApplication.class, args);
+		System.out.println("started the basic sb jdbc learning app....");
+	}
+
+}
---
+++ src/test/java/com/shantesh/learning/spring_boot_jdbc_jpa_hibernate/SpringBootJdbcJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shantesh.learning.spring_boot_jdbc_jpa_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJdbcJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
