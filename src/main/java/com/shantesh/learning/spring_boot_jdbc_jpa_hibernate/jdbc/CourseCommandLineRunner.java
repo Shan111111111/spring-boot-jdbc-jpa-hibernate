e7bdf8c4eb1eb08de3d0cac4edf58c90c5ed8dca
Shantesh <shanthu1804@gmail.com>
2024-07-26 12:14:02 +0530

    implemented SPRING JPA as here even entitymanager also removed and just interface created and extended the jparepository :p

--- src/main/java/com/shantesh/learning/spring_boot_jdbc_jpa_hibernate/jdbc/CourseCommandLineRunner.java
+++ src/main/java/com/shantesh/learning/spring_boot_jdbc_jpa_hibernate/jdbc/CourseCommandLineRunner.java
@@ -11,16 +11,16 @@ import com.shantesh.learning.spring_boot_jdbc_jpa_hibernate.pojo.Course;
 public class CourseCommandLineRunner implements CommandLineRunner {
 
 	@Autowired
-	private MYCourseJPARepository myCourseJPARepository; //changed to coursejparepository and deleted the jdbcjparepository, 
+	private MYCourseJPARepository myCourseJPARepository; 
 
 	@Override
 	public void run(String... args) throws Exception {
 
-		myCourseJPARepository.insert(new Course(234, "Jpa samuel value", "jpa Some Author vlaue"));
-		myCourseJPARepository.insert(new Course(235, "Jpa somevalue value", "jpa Some Autdsdsdsdhor"));
-		myCourseJPARepository.insert(new Course(24, "Jpa asf value", "Some dfdfAuthor"));
+		myCourseJPARepository.save(new Course(234, "spring Jpa samuel value", "spring jpa Some Author vlaue"));
+		myCourseJPARepository.save(new Course(235, "spring Jpa somevalue value", "spring jpa Some Autdsdsdsdhor"));
+		myCourseJPARepository.save(new Course(24, "spring Jpa asf value", "spring jpa Some dfdfAuthor"));
 		
-		myCourseJPARepository.delete(235);
+		myCourseJPARepository.deleteById(235);
 
 		System.out.println(myCourseJPARepository.findById(24));
 		System.out.println(myCourseJPARepository.findById(234));
--- src/main/java/com/shantesh/learning/spring_boot_jdbc_jpa_hibernate/jpa/MYCourseJPARepository.java
+++ src/main/java/com/shantesh/learning/spring_boot_jdbc_jpa_hibernate/jpa/MYCourseJPARepository.java
@@ -1,31 +1,7 @@
 package com.shantesh.learning.spring_boot_jdbc_jpa_hibernate.jpa;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.shantesh.learning.spring_boot_jdbc_jpa_hibernate.pojo.Course;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import jakarta.transaction.Transactional;
-
-@Repository
-@Transactional
-public class MYCourseJPARepository {
-	
-	@PersistenceContext
-	private EntityManager entityManager;
-
-	public void delete(long id) {
-		Course course = entityManager.find(Course.class, id);
-		entityManager.remove(course);
-	}
-
-	public Course findById(long id) {
-		return entityManager.find(Course.class, id);
-	}
-
-	public void insert(Course course) {
-		entityManager.merge(course);
-	}
-
-}
+public interface MYCourseJPARepository extends JpaRepository<Course, Integer>{}
